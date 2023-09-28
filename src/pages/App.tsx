7fc5a8152e041096e71e7eeebc86b9ca35ed46d2
Jesus Fuentes <jaff7592@gmail.com>
2023-09-28 18:19:04 -0500

    add fix for react route navigation

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ import { MenuComponent } from "../components/common/MenuComponent";
 import { FooterComponent } from "../components/common/FooterComponent";
 import { HomePageBodyComponent } from "../components/home/HomePageBodyComponent";
 import { ContactPageBodyComponent } from "../components/contact/ContactPageBodyComponent";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PythonHistoryBodyComponent } from "../components/blog/PythonHistoryBodyComponent";
 import { MeetGuidoVanRossumBodyComponent } from "../components/blog/MeetGuidoVanRossumBodyComponent";
 import { FirstStepsInDataScienceBodyComponent } from "../components/blog/FirstStepsInDataScienceBodyComponent";
@@ -17,17 +17,17 @@ export function App() {
         <>
             <div className="body">
                 <MenuComponent/>
-                <Routes>
-                    <Route path="/" element={<HomePageBodyComponent/>} />
-                    <Route path="/contacto" element={<ContactPageBodyComponent/>} />
-                    <Route path="/blog/historia-python" element={<PythonHistoryBodyComponent/>} />
-                    <Route path="/blog/conoce-guido-van-rossum" element={<MeetGuidoVanRossumBodyComponent/>} />
-                    <Route path="/blog/como-empezar-en-ciencia-de-datos-con-python" element={<FirstStepsInDataScienceBodyComponent/>} />
-                    <Route path="/blog/vale-la-pena-estudiar-python" element={<WorthsStudyPythonBodyComponent/>} />
-                    <Route path="/blog/usar-flask-aun-es-buena-opcion" element={<FlaskHasAGoodOptionBodyComponent/>} />
-                    <Route path="/blog/conoce-el-lenguaje-julia" element={<JuliaProgrammingLanguageBodyComponent/>} />
-                    <Route path="/*" element={ <Navigate to="/"/> }/>
-                </Routes>
+                  <Routes>
+                            <Route path="/" element={<HomePageBodyComponent/>} />
+                            <Route path="/contacto" element={<ContactPageBodyComponent/>} />
+                            <Route path="/blog/historia-python" element={<PythonHistoryBodyComponent/>} />
+                            <Route path="/blog/conoce-guido-van-rossum" element={<MeetGuidoVanRossumBodyComponent/>} />
+                            <Route path="/blog/como-empezar-en-ciencia-de-datos-con-python" element={<FirstStepsInDataScienceBodyComponent/>} />
+                            <Route path="/blog/vale-la-pena-estudiar-python" element={<WorthsStudyPythonBodyComponent/>} />
+                            <Route path="/blog/usar-flask-aun-es-buena-opcion" element={<FlaskHasAGoodOptionBodyComponent/>} />
+                            <Route path="/blog/conoce-el-lenguaje-julia" element={<JuliaProgrammingLanguageBodyComponent/>} />
+                            <Route path="/*" element={ <Navigate to="/"/> }/>
+                        </Routes>
                 <FooterComponent/>
             </div>
         </>
